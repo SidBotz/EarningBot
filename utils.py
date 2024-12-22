4db98148ada0f2eeef3b542973054da7fd66efde
SidBotz <sidop148@gmail.com>
2024-12-23 04:22:44 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,28 @@
+import logging, asyncio, os, re, random, pytz, aiohttp, requests, string, json, http.client
+rom bs4 import BeautifulSoup
+from shortzy import Shortzy
+from datetime import datetime, date
+from typing import List
+
+        
+async def get_verify_shorted_link(link, url, api):
+    API = api
+    URL = url
+    if URL == "api.shareus.io":
+        url = f'https://{URL}/easy_api'
+        params = {
+            "key": API,
+            "link": link,
+        }
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                    data = await response.text()
+                    return data
+        except Exception as e:
+            logger.error(e)
+            return link
+    else:
+        shortzy = Shortzy(api_key=API, base_site=URL)
+        link = await shortzy.convert(link)
+        return link

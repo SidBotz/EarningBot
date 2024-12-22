0115a19b2887364ed81a984aa276943be7f25958
SidBotz <sidop148@gmail.com>
2024-12-22 23:46:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,3 +18,4 @@ DB_NAME = os.environ.get("DB_NAME", "EarningBot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+RefferalAmount = os.environ.get("RefferalAmount", "Upto 5 ₹")
